9c41e7b995ae99b2c89c4401ea31301573ef9359
Do Ho <hsnd_1739@q.com>
2022-04-05 00:31:10 -0600

    commit video 1 notes

--- conditionsAndLoops.js
+++ conditionsAndLoops.js
@@ -1,1 +1,8 @@
-console.log('Hello World');
+// Boolean operators: ==  >  <  >=  <=  Special operators: &&  ||
+// 5 == 5 is a boolean expression that evaluates to true; 5 == 4 is a boolean expression that evaluates to false
+// 5 > 4 evaluates to true
+// && evaluates to true if both sides of it are true; so this needs a boolean value or expression on either side
+   // 5 > 4 && 4 < 10  this evaluates to true
+   // 5 > 4 && 4 > 10 this whole expression evaluates to false
+//  || if one OR the other is true then expression evlauates to true
+   // 4 > 10 || 1 == 1 left side is false but right side is true so whole expression is true
